8ee18c8381b5615b8be2d2f96c49a2d7c680c1d4
Jdelrio <julien.delrio@gmail.com>
2013-11-10 14:21:12 +0100

    Prevent activityNotFound

--- sources/src/com/juliendelrio/kitkat4taug/subjects/SubjectfFragmentNewSMSFramework.java
+++ sources/src/com/juliendelrio/kitkat4taug/subjects/SubjectfFragmentNewSMSFramework.java
@@ -1,15 +1,21 @@
 package com.juliendelrio.kitkat4taug.subjects;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.support.v4.widget.CursorAdapter;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,12 +25,52 @@ import com.juliendelrio.kitkat4taug.R;
 public class SubjectfFragmentNewSMSFramework extends AbstractSubjectfFragment {
 
 	private ListView listView;
+	private EditText editTextReceiver;
+	private EditText editTextMessage;
+	private Button buttonSendManager;
+	private Button buttonSendApi;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		listView = (ListView) inflater.inflate(R.layout.fragment_newsms_root, container, false);
 		View header = inflater.inflate(R.layout.fragment_newsms_header, listView, false);
 		listView.addHeaderView(header);
+
+		// header
+		editTextReceiver = (EditText) header.findViewById(R.id.editText_receiver);
+		editTextMessage = (EditText) header.findViewById(R.id.editText_message);
+		buttonSendManager = (Button) header.findViewById(R.id.button_send_manager);
+		buttonSendManager.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				SmsManager smsManager = SmsManager.getDefault();
+				String phoneNumber = editTextReceiver.getText().toString();
+				String smsMessage = editTextMessage.getText().toString();
+				smsManager.sendTextMessage(phoneNumber, null, smsMessage, null, null);
+			}
+		});
+		buttonSendApi = (Button) header.findViewById(R.id.button_send_api);
+		buttonSendApi.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+				String phoneNumber = editTextReceiver.getText().toString();
+				String smsMessage = editTextMessage.getText().toString();
+				smsIntent.setType("vnd.android-dir/mms-sms");
+				smsIntent.putExtra("address", phoneNumber);
+				smsIntent.putExtra("sms_body", smsMessage);
+				try {
+					startActivity(smsIntent);
+				} catch (ActivityNotFoundException e) {
+					Toast.makeText(getActivity(), R.string.no_app_to_handle, Toast.LENGTH_SHORT)
+							.show();
+				}
+			}
+		});
+
+		// list content
 		Uri uriSms = Uri.parse("content://sms/inbox");
 		Cursor c = inflater.getContext().getContentResolver().query(uriSms, null, null, null, null);
 		listView.setAdapter(new CustomAdapter(inflater.getContext(), c,
